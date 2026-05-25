5469b4d797fc04cbbedf2e25a673a7a574d93c2e
animesh <animesh@animeshs-MacBook-Air-2.local>
2026-05-25 13:15:55 -0700

    chore: remove Next.js config files for Vite-only deployment

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "standalone",
-};
-
-export default nextConfig;
-
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
